900c19dd955f806ae97bf0fba140a51aae80e7f2
tcpatch <tcpatch2014@gmail.com>
2018-02-11 16:05:24 -0500

    20180211 TP

--- scraping_ispr.py
+++ scraping_ispr.py
@@ -2,14 +2,20 @@ from urllib.request import Request, urlopen
 
 
 def scrape_ispr():
-    base_url = r'https://www.ispr.gov.pk/front/main.asp?o=t-press_release&id='
-    end_url = '&cat=army'
-    index = 4010
-    for index in range(1000, 4999):
-        full_url = base_url + str(index) + end_url
-        req = Request(full_url, headers={'User-Agent': 'Mozilla/5.0'})
-        webpage = urlopen(req).read()
-        print(webpage)
+    #This function scrapes the ispr website and saves the raw HTML to a text file for future processing
+    base_url = r'https://www.ispr.gov.pk/front/main.asp?o=t-press_release&cat=army&date='
+    years = ['2008', '2009']
+    months = ['1', '2']
+    days = ['1', '2']
+    for year in years:
+        for month in months:
+            for day in days:
+                full_url = base_url + str(year) + r'/' + str(month) + r'/' + str(day)
+                req = Request(full_url, headers={'User-Agent': 'Mozilla/5.0'})
+                webpage = urlopen(req).read()
+                if len(webpage) > 0:
+                    with open(str(year) + str(month) + str(day) + '.txt', 'a') as fp:
+                        fp.write(webpage.encode('utf-8') + '\n')
 
 
 if __name__ == '__main__':
