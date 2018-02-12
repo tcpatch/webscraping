763cdcf0c2215249b5914f208de2d7afe40f0c39
tcpatch <tcpatch2014@gmail.com>
2018-02-11 21:45:51 -0500

    20180211 TP regex fix

--- scraping_ispr.py
+++ scraping_ispr.py
@@ -1,6 +1,7 @@
 from urllib.request import Request, urlopen
 import re
 
+
 def scrape_ispr():
     #This function scrapes the ispr website and if the word missile is in the raw HTML the url will be printed
     base_url = r'https://www.ispr.gov.pk/front/main.asp?o=t-press_release&cat=army&date='
@@ -17,14 +18,11 @@ def scrape_ispr():
                     req = Request(full_url, headers={'User-Agent': 'Mozilla/5.0'})
                     webpage = urlopen(req).read()
                     content = str(webpage)
-                    m = re.search(r'[missile]', content)
+                    m = re.search(r'(missile|Missile)', content)
                     if m:
                         print(full_url)
                 except:
                     pass
-#                if len(webpage) > 0:
-#                    with open(str(year) + str(month) + str(day) + '.txt', 'a') as fp:
-#                        fp.write(webpage.encode('utf-8') + '\n')
 
 
 if __name__ == '__main__':
